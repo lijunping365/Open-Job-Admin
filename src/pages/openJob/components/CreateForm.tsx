c5717909a560be9a0513a1b5f5c388963209e807
lijunping <2544054976@qq.com>
2022-04-17 12:26:31 +0800

    feat: 优化

--- src/pages/openJob/components/CreateForm.tsx
+++ src/pages/openJob/components/CreateForm.tsx
@@ -62,7 +62,7 @@ const CreateForm: React.FC<CreateFormProps> = (props) => {
         <Row>
           <Col span={12}>
             <FormItem
-              name="name"
+              name="jobName"
               label="任务名称"
               rules={[{ required: true, message: '请输入任务名称！' }]}
             >
@@ -71,20 +71,11 @@ const CreateForm: React.FC<CreateFormProps> = (props) => {
           </Col>
           <Col span={12}>
             <FormItem
-              name="cronExpression"
-              label="Cron 表达式"
-              rules={[{ required: true, message: '请输入Cron 表达式！' }]}>
-              <Input.Group compact style={{display: 'flex'}}>
-                <Input placeholder="请输入Cron 表达式"  value={cronExpressValue} onChange={(e)=>setCronExpressValue(e.target.value)}/>
-                <Button
-                  type="primary"
-                  onClick={() => {
-                    handleCronModalVisible(true);
-                  }}
-                >
-                  Cron 工具
-                </Button>
-              </Input.Group>
+              name="handlerName"
+              label="handlerName"
+              rules={[{ required: true, message: '请输入handlerName！' }]}
+            >
+              <Input placeholder="请输入handlerName" />
             </FormItem>
           </Col>
         </Row>
@@ -96,6 +87,24 @@ const CreateForm: React.FC<CreateFormProps> = (props) => {
               label="任务参数"
             >
               <TextArea rows={4}  placeholder="请输入任务参数（json 格式）" />
+            </FormItem>
+          </Col>
+          <Col span={12}>
+            <FormItem
+              name="cronExpression"
+              label="Cron 表达式"
+            >
+              <Input.Group compact style={{display: 'flex'}}>
+                <Input placeholder="请输入Cron 表达式"  value={cronExpressValue} onChange={(e)=>setCronExpressValue(e.target.value)}/>
+                <Button
+                  type="primary"
+                  onClick={() => {
+                    handleCronModalVisible(true);
+                  }}
+                >
+                  Cron 工具
+                </Button>
+              </Input.Group>
             </FormItem>
           </Col>
         </Row>
